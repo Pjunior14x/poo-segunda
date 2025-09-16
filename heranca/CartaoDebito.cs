a60657961ba9b82639c7dc9d4e9a5548d90d4dd1
Paulo Junior <162725435+Pjunior14x@users.noreply.github.com>
2025-09-16 00:10:50 +0000

    feat:Cartao de debito

---
+++ heranca/CartaoDebito.cs
@@ -0,0 +1,30 @@
+public class CartaoDebito
+{
+
+    public CartaoDebito()
+    {
+        Bandeira = "Visa";
+        Bandeira = "MinhaBandeira";
+        Bandeira = BandeiraCartao.Mastercard;
+
+    }
+    public string Numero { get; set; }
+    public BandeiraCartao Bandeira { get; set; }
+
+    public DateTime Vencimento { get; set; }
+
+    public string Portador { get; set; }
+
+    public string Cvv { get; set; }
+
+}
+
+public enum BandeiraCartao
+{
+
+Visa,
+Mastercard,
+Amex,
+Elo
+
+}
